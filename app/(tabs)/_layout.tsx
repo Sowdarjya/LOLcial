f60f895ee27d6c3547100068b25119557a9ce6b0
Sowdarjya <kolaysowdarjya@gmail.com>
2025-05-27 12:58:18 +0530

    Add tab navigation layout and screens for index, create, notifications, and profile

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,48 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import React from "react";
+
+const TabLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="add-circle" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="heart" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="person-circle" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabLayout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,8 @@
-import { Image, Pressable, Text, TouchableOpacity, View } from "react-native";
-import { styles } from "../styles/auth.styles";
+import { Redirect } from "expo-router";
+import React from "react";
 
-export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
-      <TouchableOpacity onPress={() => alert("sexy boi")}>
-        <Text>Press me</Text>
-      </TouchableOpacity>
-      <Pressable onPress={() => alert("sexy boi")}>
-        <Text>Press me 2</Text>
-      </Pressable>
-      <Image
-        source={require("../assets/images/icon.png")}
-        style={{
-          width: 100,
-          height: 100,
-          marginTop: 20,
-          borderRadius: 50,
-        }}
-      />
-    </View>
-  );
-}
+const index = () => {
+  return <Redirect href="/(tabs)" />;
+};
+
+export default index;
