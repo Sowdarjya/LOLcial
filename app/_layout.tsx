bcf069947c6d842b867ee30e3d363dc4b1115f30
Sowdarjya <kolaysowdarjya@gmail.com>
2025-06-06 11:39:25 +0530

    Add Convex functions and configuration files
    - Created README.md for Convex functions with examples for query and mutation functions.
    - Generated TypeScript definitions and JavaScript utilities for Convex API, data model, and server functions.
    - Added authentication configuration for Clerk.
    - Created tsconfig.json for TypeScript project settings.
    - Updated package.json and package-lock.json to include Convex and its dependencies.
    - Implemented ClerkAndConvexProvider component for integrating Clerk with Convex.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,15 @@
-import InitialLayout from "@/components/initialLayout";
-import { ClerkProvider } from "@clerk/clerk-expo";
-import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import InitialLayout from "@/components/InitialLayout";
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider
-      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
-      tokenCache={tokenCache}
-    >
+    <ClerkAndConvexProvider>
       <SafeAreaProvider>
         <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
           <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
-    </ClerkProvider>
+    </ClerkAndConvexProvider>
   );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://smart-labrador-34.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
